df90666cf925d3664454ef024ee8ac71dd7257f4
Kevin Chiu <kevin@readmoo.com>
2013-08-19 17:14:54 +0800

    Add lost libraries (zip and zipfs)

--- epub-modules/simple-readium-js/build.js
+++ epub-modules/simple-readium-js/build.js
@@ -13,6 +13,8 @@
         underscore: '../../lib/underscore-1.4.4',
         backbone: '../../lib/backbone-0.9.10',
         URIjs: '../../lib/URIjs',
+        zip: '../../lib/zip',
+        zipfs: '../../lib/zip-fs',
         'cfi_module': '../../epub-cfi/min/cfi_module.min',
         'epub_fetch_module': '../../epub-fetch/min/epub_fetch_module.min',
         'epub_parser_module': '../../epub-parser/min/epub_parser_module.min',
@@ -22,5 +24,5 @@
         'epub_fixed_module': '../../epub-fixed/min/epub_fixed_module.min',
         'epub_reading_system': '../../epub-ers/src/epub_reading_system'
     },
-    exclude: ['jquery', 'underscore', 'backbone', 'URIjs/URI']
+    exclude: ['jquery', 'underscore', 'backbone', 'URIjs/URI', 'zip', 'zipfs']
 })
